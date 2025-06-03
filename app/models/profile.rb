5db5bf8e4ae7dfba6ff6d0828b883f98c8ed8c2b
Giuseppe Varriale <pepe.saoleo@gmail.com>
2025-06-03 18:21:28 -0300

    refact: ajustes

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -5,7 +5,16 @@ class Profile < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
 
+  before_save :strip_names
+
   def full_name
     "#{first_name} #{last_name}"
+  end
+
+  private
+
+  def strip_names
+    self.first_name = first_name.strip if first_name.present?
+    self.last_name = last_name.strip if last_name.present?
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,4 @@ class User < ApplicationRecord
 
   has_one :profile, dependent: :destroy
   accepts_nested_attributes_for :profile
-
-  after_create :create_profile
-
-  def create_profile
-    build_profile.save unless profile.present?
-  end
 end
