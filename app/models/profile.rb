83f5aae4a02a13b83647564584f7983c81340cc5
Giuseppe Varriale <pepe.saoleo@gmail.com>
2025-06-03 15:34:06 -0300

    feat: melhora validações do cadastro

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,9 @@ class Profile < ApplicationRecord
   belongs_to :user
   has_one_attached :avatar
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
   def full_name
     "#{first_name} #{last_name}"
   end
