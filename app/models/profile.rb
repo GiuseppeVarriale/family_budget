c671e1695c565c1bac1e9b628697b518a1ef93d8
Giuseppe Varriale <pepe.saoleo@gmail.com>
2025-06-03 15:03:17 -0300

    feat: versão inicial profile

---
+++ app/models/profile.rb
@@ -0,0 +1,8 @@
+class Profile < ApplicationRecord
+  belongs_to :user
+  has_one_attached :avatar
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one :profile, dependent: :destroy
+  accepts_nested_attributes_for :profile
+
+  after_create :create_profile
+
+  def create_profile
+    build_profile.save unless profile.present?
+  end
 end
